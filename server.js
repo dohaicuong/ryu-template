f4c93ff1f2b97a81e1f7396fbb2337acf7b9299f
Eric Do <beatyshot@gmail.com>
2022-05-31 23:39:15 +1000

    setup project

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const { engine } = require('express-handlebars')
+
+const { PrismaClient } = require('@prisma/client')
+
+const app = express()
+const prisma = new PrismaClient()
+
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', './views')
+
+app.get('/', async (req, res) => {
+  const users = await prisma.user.findMany()
+  res.render('home', {
+    layout: false,
+    helpers: {
+      firstName() { return users[0].firstName },
+      lastName() { return users[0].lastName }
+    }
+  })
+})
+
+app.get('/create-user', async (req, res) => {
+  const newUser = await prisma.user.create({
+    data: {
+      firstName: 'Ryu',
+      lastName: 'Nguyen'
+    }
+  })
+
+  res.send({ user: newUser })
+})
+
+app.listen(3000, () => {
+  console.log('http://localhost:3000')
+})
